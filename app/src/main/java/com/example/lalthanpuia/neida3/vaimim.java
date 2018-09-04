515b0fe3790fad22f25ff454a08fe2e19d2a6e69
lalthanpuia <thanpuia46@gmail.com>
2018-09-04 11:49:00 +0530

    adding vaimim

--- app/src/main/java/com/example/lalthanpuia/neida3/vaimim.java
+++ app/src/main/java/com/example/lalthanpuia/neida3/vaimim.java
@@ -1,5 +1,6 @@
 package com.example.lalthanpuia.neida3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,18 +14,28 @@ public class vaimim extends AppCompatActivity {
     }
 
     public void natna1_Click(View view) {
+        Intent intent = new Intent(this,Main61Activity_vaimim_KuangTawih.class);
+        startActivity(intent);
     }
 
     public void natna2_Click(View view) {
+        Intent intent = new Intent(this,Main62Activity_vaimim_a_kuang_meihawl.class);
+        startActivity(intent);
     }
 
     public void natna3_click(View view) {
+        Intent intent = new Intent(this,Main63Activity_vaimim_hnah_rau.class);
+        startActivity(intent);
     }
 
     public void natna4_Click(View view) {
+        Intent intent = new Intent(this,Main63Activity_vaimim_Hnah_rau_val.class);
+        startActivity(intent);
     }
 
     public void hri1_Click(View view) {
+        Intent intent = new Intent(this,Main65Activity_vaimim_kau_eitu.class);
+        startActivity(intent);
     }
 
     public void hri2_click(View view) {
