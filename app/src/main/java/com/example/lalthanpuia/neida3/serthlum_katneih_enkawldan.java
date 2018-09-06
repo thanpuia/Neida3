17720c5b465480aec22db9a790e95d63cd7b988a
lalthanpuia <thanpuia46@gmail.com>
2018-09-06 21:12:19 +0530

    thil thu lo anga gaih ho delete hma

---
+++ app/src/main/java/com/example/lalthanpuia/neida3/serthlum_katneih_enkawldan.java
@@ -0,0 +1,29 @@
+package com.example.lalthanpuia.neida3;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class serthlum_katneih_enkawldan extends Fragment {
+
+
+    public serthlum_katneih_enkawldan() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_serthlum_katneih_enkawldan, container, false);
+    }
+
+}
